04eb05bb4b8343d50008c209dcdb4d738679a14c
Ekata <yogaelen108@gmail.com>
2019-09-15 20:38:01 +0100

    added object for chart constants

--- js/stat.js
+++ js/stat.js
@@ -8,9 +8,11 @@ var cloudParams = {
   SHADOW_GAP: 10
 };
 
-var COLUMN_GAP = 50;
-var BAR_WIDTH = 40;
-var barHeight = 150;
+var chartParams = {
+  COLUMN_GAP: 50,
+  COLUMN_WIDTH: 40,
+  COLUMN_HEIGHT: 150
+};
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -33,28 +35,28 @@ window.renderStatistics = function (ctx, names, times) {
 
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', cloudParams.X_POSITION + COLUMN_GAP, cloudParams.Y_POSITION + COLUMN_GAP / 2);
-  ctx.fillText('Cписок результатов:', cloudParams.X_POSITION + COLUMN_GAP, cloudParams.Y_POSITION + COLUMN_GAP);
+  ctx.fillText('Ура вы победили!', cloudParams.X_POSITION + chartParams.COLUMN_GAP, cloudParams.Y_POSITION + chartParams.COLUMN_GAP / 2);
+  ctx.fillText('Cписок результатов:', cloudParams.X_POSITION + chartParams.COLUMN_GAP, cloudParams.Y_POSITION + chartParams.COLUMN_GAP);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     var saturationPersntage = Math.floor(Math.random() * 100);
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], cloudParams.X_POSITION + COLUMN_GAP + (COLUMN_GAP + BAR_WIDTH) * i, cloudParams.HEIGHT - cloudParams.Y_POSITION * 1);
+    ctx.fillText(names[i], cloudParams.X_POSITION + chartParams.COLUMN_GAP + (chartParams.COLUMN_GAP + chartParams.COLUMN_WIDTH) * i, cloudParams.HEIGHT - cloudParams.Y_POSITION * 1);
 
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(cloudParams.X_POSITION + COLUMN_GAP + (COLUMN_GAP + BAR_WIDTH) * i, cloudParams.HEIGHT - cloudParams.Y_POSITION * 3, BAR_WIDTH, -((barHeight * times[i]) / maxTime));
+      ctx.fillRect(cloudParams.X_POSITION + chartParams.COLUMN_GAP + (chartParams.COLUMN_GAP + chartParams.COLUMN_WIDTH) * i, cloudParams.HEIGHT - cloudParams.Y_POSITION * 3, chartParams.COLUMN_WIDTH, -((chartParams.COLUMN_HEIGHT * times[i]) / maxTime));
     } else {
     // ctx.fillStyle = `hsl(240, ${Math.floor(Math.random() * 100)}%, 50%)`;
       ctx.fillStyle = 'hsl(240, ' + saturationPersntage + '%, 50%)';
-      ctx.fillRect(cloudParams.X_POSITION + COLUMN_GAP + (COLUMN_GAP + BAR_WIDTH) * i, cloudParams.HEIGHT - cloudParams.Y_POSITION * 3, BAR_WIDTH, -((barHeight * times[i]) / maxTime));
+      ctx.fillRect(cloudParams.X_POSITION + chartParams.COLUMN_GAP + (chartParams.COLUMN_GAP + chartParams.COLUMN_WIDTH) * i, cloudParams.HEIGHT - cloudParams.Y_POSITION * 3, chartParams.COLUMN_WIDTH, -((chartParams.COLUMN_HEIGHT * times[i]) / maxTime));
     }
   }
 
   for (var j = 0; j < times.length; j++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.floor(times[j]), cloudParams.X_POSITION + COLUMN_GAP + (COLUMN_GAP + BAR_WIDTH) * j, cloudParams.HEIGHT - ((barHeight * times[j]) / maxTime) - cloudParams.Y_POSITION * 4);
+    ctx.fillText(Math.floor(times[j]), cloudParams.X_POSITION + chartParams.COLUMN_GAP + (chartParams.COLUMN_GAP + chartParams.COLUMN_WIDTH) * j, cloudParams.HEIGHT - ((chartParams.COLUMN_HEIGHT * times[j]) / maxTime) - cloudParams.Y_POSITION * 4);
   }
 };
