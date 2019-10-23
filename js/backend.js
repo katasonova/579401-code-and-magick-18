290764d051bf5bd97c23604ce8099d7d72f7e75d
Ekata <yogaelen108@gmail.com>
2019-10-23 22:00:46 +0100

    fixed linting errors

--- js/backend.js
+++ js/backend.js
@@ -1,47 +1,47 @@
 'use strict';
 (function () {
- var SUCCESS_STATUS = 200;
- var TIMEOUT = 10000;
- var URL = {
-   SAVE: 'https://js.dump.academy/code-and-magick',
-   LOAD: 'https://js.dump.academy/code-and-magick/data'
- };
+  var SUCCESS_STATUS = 200;
+  var TIMEOUT = 10000;
+  var URL = {
+    SAVE: 'https://js.dump.academy/code-and-magick',
+    LOAD: 'https://js.dump.academy/code-and-magick/data'
+  };
 
 
- var genericServerInteracttions = function (onLoad, onError, xhr) {
-   xhr.responseType = 'json';
-   xhr.addEventListener('load', function () {
-     if (xhr.status === SUCCESS_STATUS) {
-       onLoad(xhr.response);
-     } else {
-       onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-     }
-   });
-   xhr.addEventListener('error', function () {
-     onError('Произошла ошибка соединения');
-   });
-   xhr.addEventListener('timeout', function () {
-     onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-   });
-   xhr.timeout = TIMEOUT;
- };
+  var genericServerInteracttions = function (onLoad, onError, xhr) {
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === SUCCESS_STATUS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+    xhr.timeout = TIMEOUT;
+  };
 
- var load = function (onLoad, onError) {
-  var xhr = new XMLHttpRequest();
-   genericServerInteracttions(onLoad, onError, xhr);
-   xhr.open('GET', URL.LOAD);
-   xhr.send();
- };
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    genericServerInteracttions(onLoad, onError, xhr);
+    xhr.open('GET', URL.LOAD);
+    xhr.send();
+  };
 
- var save = function (data, onLoad, onError) {
-  var xhr = new XMLHttpRequest();
-   genericServerInteracttions(onLoad, onError, xhr);
-   xhr.open('POST', URL.SAVE);
-   xhr.send(data);
- };
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    genericServerInteracttions(onLoad, onError, xhr);
+    xhr.open('POST', URL.SAVE);
+    xhr.send(data);
+  };
 
- window.backend = {
-   load: load,
-   save: save
- };
+  window.backend = {
+    load: load,
+    save: save
+  };
 })();
